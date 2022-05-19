58424c36cd61f777d115ecb5a8854ea0c4101275
mohdhussnainraza <92803788+mohdhussnainraza@users.noreply.github.com>
2022-05-19 10:47:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,36 @@
-var las_position_of_x, last_position_of_y;
-color ="blank";
-width_of_line = 2;
-
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d");
-
-var width = screen.width;
-new_width = screen.width - 70;
-new_height = screen.height - 300;
-    if(width < 992)
-    {
-    document.getElementById("myCanvas").width = new_width;
-    document.getElementById("myCanvas").height = new_height;
-    document.body.style.overflow = "hidden";
-    }
-
-    canvas.addEventlistener("touchstart", my_touchstart);
-
-    function my_touchstart(e)
-    {
-        console.log("my_touchstart");
-
-      color = document.getElementById("color").value;
-      width_of_line = document.getElementById("width_of_line").value;
-
-
-      last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
-      last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
-    }
-    canvas.addEventListener("touchmove", my_touchmove);
+menu_list_array = ["Veg Margherita Pizza",
+                        "Chicken Tandoori Pizza",
+                        "Veg Supreme Pizza",
+                        "Paneer Tikka Pizza",
+                        "Deluxe Veggie Pizza",
+                        "Veg Extravaganza Pizza"];
     
+    function getmenu(){
+        var htmldata="";
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu 
+    }
 
-    function my_touchmove(e)
-    {
+    function add_item(){
+        var htmldata;
+		var imgtags='<img id="im1" src="pizzaImg.png">'
+        var item=document.getElementById("add_item").value;
+        // use the sort function as - menu_list_array.sort();
+        menu_list_array.sort();
+        htmldata="";
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;
+		
+    }
 
-        console.log("my_touchMove");
-        current_position_of_touch_x = e.touches[0].clientY - canvas.offsetLeft;
-        current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
-
-
-        ctx.beginPath();
-        ctx.strokeStyle = color
+function add_top(){
+	var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
